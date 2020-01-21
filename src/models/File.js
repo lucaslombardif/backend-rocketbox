e7736cecef8bbc4fc61e58e6d89c9fbe1568fa8a
lucaslombardif <lucaas-floriano@hotmail.com>
2020-01-21 14:49:18 -0300

    Estrutura final do Backend

---
+++ src/models/File.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const File = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    path: {
+        type: String,
+        requried: true
+    },
+}, {
+    timestamps: true,
+    toObject: { virtuals: true },
+    toJSON: { virtuals: true }
+});
+
+File.virtual('url').get(function () {
+    const url = process.env.URL || 'http://localhost:3333'
+
+
+    return `${url}/files/${encodeURIComponent(this.path)}`;
+});
+
+module.exports = mongoose.model("File", File);
---
+++ src/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+io.on('connection', socket => {
+    socket.on('connectRoom', box => {
+        socket.join(box);
+    })
+});
+
+mongoose.connect('mongodb+srv://omnistacksix:omnistacksix@cluster0-vyjn1.mongodb.net/sixomnistack?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+);
+
+app.use((req, res, next) => {
+    req.io = io;
+
+    return next();
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')));
+
+app.use(require('./routes'));
+
+server.listen(process.env.PORT || 3333);
